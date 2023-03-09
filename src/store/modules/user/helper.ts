00c151589ea37a056478f56537b48965386e8edd
Wenlong Xue <>
2023-03-09 10:18:12 +0800

    修改默认提示

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/76082537?v=4',
+      name: '',
+      description: '<a href="https://www.cnsre.cn" class="text-blue-500" target="_blank" >SRE运维博客</a>',
     },
   }
 }
